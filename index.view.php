2a30f04662ef64148e186c310aa8cd74845ad210
Steven Antonio <steven@stevenanton.io>
2018-04-27 02:31:00 -0700

    Initial commit

---
+++ index.view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>gitHost</title>
+  <link rel="stylesheet" href="styles.css" type="text/css">
+  </head>
+  <body>
+
+  </body>
+</html>
