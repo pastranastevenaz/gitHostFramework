2fe52bc09b396e9fb4a373c3bee3cc03b29aa66e
Steven Antonio <steven@stevenanton.io>
2018-04-30 06:11:03 -0700

    add mobile browser style

--- layouts/default.php
+++ layouts/default.php
@@ -5,9 +5,17 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>GitHosts</title>
-  <link rel="stylesheet" href="styles.css" type="text/css">
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <!-- Chrome, Firefox OS and Opera -->
+  <meta name="theme-color" content="#333">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="styles.css" type="text/css">
+  <link rel="stylesheet" media="(max-width:600px)" href="small.css">
+  <link rel="stylesheet" media="(min-width:601px)" href="large.css">
   </head>
   <body>
 
